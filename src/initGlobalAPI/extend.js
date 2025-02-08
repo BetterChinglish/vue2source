792bf52e3ef89d9e68c2beb0f877911db5bffa14
BetterChinglishCompany <zhangjunjie73@huawei.com>
2025-02-08 11:01:59 +0800

    extend方法创建子类

--- src/initGlobalAPI/extend.js
+++ src/initGlobalAPI/extend.js
@@ -1,12 +1,22 @@
+import { mergeOptions } from "../utils";
+
+
 export default function (Vue) {
+  // 创建子类 继承于父类， 拓展的时候将父组件的东西拓展到自己身上
   Vue.extend = function (extendOptions) {
-    // const Super = this;
-    // const Sub = function VueComponent(options) {
-    //   this._init(options);
-    // }
-    // Sub.prototype = Object.create(Super.prototype);
-    // Sub.prototype.constructor = Sub;
-    // Sub.options = extendOptions;
-    // return Sub;
+    // 创建子类
+    const Sub = function VueComponent(options) {
+      // 使用Vue的_init初始化子组件类
+      this._init(options);
+    }
+    Sub.prototype = Object.create(this.prototype);
+    Sub.prototype.constructor = Sub;
+    
+    Sub.options = mergeOptions(this.options, extendOptions);
+    
+    // mxin use component
+    // Sub.mixin = this.mixin;
+    
+    return Sub;
   }
 }
